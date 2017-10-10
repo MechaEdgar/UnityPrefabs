f56da3eddc6fc91a2b5a139c102b51cc6837226a
MechaEdgar <ee1206@hotmail.com>
2017-10-10 13:37:39 -0500

    Particles created
    So, the idea its when the bullet hits the wall explotes in particles, so in this commit i create the particles, and the explosions logic.

---
+++ UnityPrefabsPractice/Assets/Explosion.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour {
+
+	public GameObject[] explosionPrefabs;
+	public int amountOfParticles = 3;
+
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i <= amountOfParticles; i++) {
+		
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
