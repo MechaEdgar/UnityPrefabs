d5cdf17cae22cdf450baa58729219ad23de03198
MechaEdgar <ee1206@hotmail.com>
2017-10-10 12:24:01 -0500

    Made a bullet,shoot that bullet,destroy that bullet.
    So, i create a scene, create a bullet prefab and add the logic to shoot a bullet everytime  click the mouse, randomize the direction and destroy the bullet in 3 seconds.

---
+++ UnityPrefabsPractice/Assets/Bullet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+	public float shoothingForce = 10f;
+	public Vector3 shoothingDirection;
+
+	public float lifeTime = 3f;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody> ().AddForce (shoothingDirection * shoothingForce);		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		lifeTime -= Time.deltaTime;
+		if (lifeTime <= 0) {
+			Destroy (gameObject);
+		}
+	}
+}
