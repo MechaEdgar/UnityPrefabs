ad235c837422e14bfcd75ba9582863347c19f821
MechaEdgar <ee1206@hotmail.com>
2017-10-11 20:51:43 -0500

    Particles created!

--- UnityPrefabsPractice/Assets/Explosion.cs
+++ UnityPrefabsPractice/Assets/Explosion.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 
 public class Explosion : MonoBehaviour {
 
-	public GameObject[] explosionPrefabs;
+	public GameObject[] particlePrefabs;
 	public int amountOfParticles = 3;
 
 
 	// Use this for initialization
 	void Start () {
-		for (int i = 0; i <= amountOfParticles; i++) {
-		
+		for (int i = 0; i < amountOfParticles; i++) {
+			GameObject particlePrefab = Instantiate(particlePrefabs[Random.Range(0,particlePrefabs.Length)]);
+			particlePrefab.transform.position = transform.position;
 		}
 	}
 	
